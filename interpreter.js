e0eef0ca591881f9b0114c4ab2abbd0138969a6e
Breno Santos <brenosantos145@gmail.com>
2023-09-11 16:59:27 -0300

    feat: adding binary operations

--- interpreter.js
+++ interpreter.js
@@ -6,6 +6,52 @@ function interpret(node, environment) {
         [node.name.text]: node.value,
       });
     }
+    case "Binary": {
+      const rhs = interpret(node.rhs, environment);
+      const lhs = interpret(node.lhs, environment);
+
+      switch (node.op) {
+        case "Add": {
+          return lhs + rhs;
+        }
+        case "Sub": {
+          return lhs - rhs;
+        }
+        case "Mul": {
+          return lhs * rhs;
+        }
+        case "Div": {
+          return lhs / rhs;
+        }
+        case "Eq": {
+          return lhs === rhs;
+        }
+        case "Neq": {
+          return lhs !== rhs;
+        }
+        case "Lt": {
+          return lhs < rhs;
+        }
+        case "Gt": {
+          return lhs > rhs;
+        }
+        case "Lte": {
+          return lhs <= rhs;
+        }
+        case "Gte": {
+          return lhs >= rhs;
+        }
+        case "And": {
+          return lhs && rhs;
+        }
+        case "Or": {
+          return lhs || rhs;
+        }
+        default: {
+          throw new Error(`Unexpected binary operator: ${node.op}`);
+        }
+      }
+    }
     case "Str": {
       return node.value;
     }
